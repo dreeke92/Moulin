ce6173f120ce79883ef9c49acdc5b02bd9f3689e
Andreas Van Assche <andreasava@hotmail.com>
2019-07-31 22:19:34 +0200

    first setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonr
+  get 'generic', to: "pages#generic"
+  get 'elements', to: "pages#elements"
 end
